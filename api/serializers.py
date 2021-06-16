f6ea2bc1111fe97b6f63d88f1c2d7d687f363f3f
austinoski <80971733+austinoski@users.noreply.github.com>
2021-06-16 02:50:46 +0100

    Create serializers.py
    Add serializers.py file to hold model serializer classes.

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+# required imports here
+
+from rest_framework.serializers import ModelSerializer
+
+from .models import Patient, HealthOfficer, Hospital, MedicalRecord
+
+
+# serializers definition here
