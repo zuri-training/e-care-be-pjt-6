e2b90f1849b534c03a8503244a4b0dba17657965
blac_dev <oreoluwa.owoborode.1035@fuoye.edu.ng>
2021-06-16 13:34:32 +0100

    Serialize HealthOfficer model in api app

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,26 @@ from .models import Patient, HealthOfficer, Hospital, MedicalRecord
 
 
 # serializers definition here
+
+class HealthOfficerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HealthOfficer
+        fields = ["user",
+                  "phone_number",
+                  "first_name",
+                  "last_name",
+                  "other_name ",
+                  "gender",
+                  "date_of_birth",
+                  "age",
+                  "specialty",
+                  "marital_status",
+                  "city",
+                  "lga",
+                  "state",
+                  "address",
+                  "last_seen",
+                  "created_at",
+                  "updated_at",
+                  "hospitals",
+                  ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+# from .models import HealthOfficer
+# from rest_framework import generics
+# from .serializers import HealthOfficerSerializer
 # Create your views here.
+
+
+# class HealthOfficerlist(generics.ListCreateAPIView):
+#     queryset = HealthOfficer.objects.all()
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+#     serializer_class = HealthOfficerSerializer
