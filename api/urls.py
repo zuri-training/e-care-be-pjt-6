f3f59fc948ad1702d65ceb55ea13412073a703a3
blacdev <oreoluwa.owoborode.1035@fuoye.edu.ng>
2021-06-19 01:01:19 -0700

    Create serializer for Health-officer model in serializer.py (#25)
    * Create serializer for Health-officer model in serializer.py
    Create CRUD for Health-officer serializer in views.py
    Create end-point for Health-officer-create-list-view and Health-officer-retrieve-update-view in api/urls.py
    
    * update Health-officer-serializer class in api/serializer.py
    
    * changes to PatientRetrieveUpdateAPIView: uncommented permission_classes

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,20 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import PatientCreateListAPIView, PatientRetrieveUpdateAPIView
+from .views import PatientCreateListAPIView, PatientRetrieveUpdateAPIView, HealthOfficerRetrieveUpdateAPIView, HealthOfficerCreateListAPIView
 
 
 urlpatterns = [
-    path('user/patients/', PatientCreateListAPIView.as_view(), name='patient-list-create'),
-    path('user/patients/<str:uuid>/', PatientRetrieveUpdateAPIView.as_view(), name='patient-get-update'),
+    path('user/patients/', PatientCreateListAPIView.as_view(),
+         name='patient-list-create'),
+    path('user/patients/<str:uuid>/',
+         PatientRetrieveUpdateAPIView.as_view(), name='patient-get-update'),
+
+
+    path("user/health-officer/", HealthOfficerCreateListAPIView.as_view(),
+         name="health-officer-list-create"),
+    path("user/health-officer/<str:uuid>/",
+         HealthOfficerRetrieveUpdateAPIView.as_view(), name="health-officer-get-update"),
 
     path('user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
