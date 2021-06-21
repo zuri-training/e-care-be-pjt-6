bee85faf766b0158267a1d1b077879c79b04adb3
blac_dev <oreoluwa.owoborode.1035@fuoye.edu.ng>
2021-06-21 09:08:24 +0100

    Reverse hidden values in settings.py

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = []
 
 
 # Application definition
