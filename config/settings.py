c189eb7a5f94d91c98074140fd21812980c56a75
Josiah Augustine <austinoski92@gmail.com>
2021-07-04 04:10:27 +0100

    Add django-environ to manage environment variables. Update CONTRIBUTION.md file

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
