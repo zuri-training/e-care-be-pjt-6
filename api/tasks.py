c21269974f901c40016b8d247525549d0f09fbbc
blacdev <stevenowoborode@gmail.com>
2021-07-04 23:46:25 +0100

    implement multithreading using celery and redis. create celery.py, tasks.py file edit settings.py, utils.py, init.py file

---
+++ api/tasks.py
@@ -0,0 +1,34 @@
+from time import sleep
+from celery.decorators import task
+from celery.utils.log import get_task_logger
+from .utils import send_mail_to
+
+
+
+logger = get_task_logger(__name__)
+
+@task(name='my_task')
+# my_first_task.delay(15)
+
+def my_first_task(request, duration):
+
+    error_message = ""
+    is_task_completed= False
+
+    try:
+        sleep(duration)
+        is_task_completed= True
+    except Exception as err:
+        error_message= str(err)
+        logger.error(error_message)
+
+    if is_task_completed:
+        send_mail_to(request)
+    else:
+        return (0)
+    return ("task done")
+
+my_first_task.delay({"email":"owoborodeseye@gmail.com", "subject":"random", "body":"dope"}, 5)
+my_first_task.delay({"email":"owoborodeseye@gmail.com", "subject":"random", "body":"dope"}, 4)
+my_first_task.delay({"email":"owoborodeseye@gmail.com", "subject":"random", "body":"dope"}, 3)
+my_first_task.delay({"email":"owoborodeseye@gmail.com", "subject":"random", "body":"dope"}, 2)
--- api/utils.py
+++ api/utils.py
@@ -1,19 +1,9 @@
 from django.core.mail import send_mail
 from config.settings import EMAIL_HOST_USER
-import json
 
-
-def json_confirmation(request):
-  try:
-    data = json.loads(str(request))
-  except ValueError:
-    return ("error")
-  return data
-
-def mail_Function(request):
-    data = json_confirmation(request)
-    reciever = data["email"]
-    subject = data["subject"]
-    Body = data["body"]
-    send_mail(subject, Body, EMAIL_HOST_USER, [reciever])
-    return ("ok")
+def send_mail_to(request):
+    sender = EMAIL_HOST_USER
+    reciever = request["email"]
+    subject = request["subject"]
+    body = request["body"]
+    send_mail(subject, body, sender, [reciever])
